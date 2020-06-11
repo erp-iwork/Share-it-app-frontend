a0906ef113dfc150eff281681c1a40d5fc24de28
Yohannes Berhanu <Johnlights51@gmail.com>
2020-06-11 06:55:51 +0300

    After Registration Page

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -2,34 +2,60 @@ import React, { Component } from "react";
 import { Col } from "reactstrap";
 // import LoginImage from "../../assets/Nunu.jpg";
 import LoginImage from "../../assets/demo-nanny.jpg";
-
 import { Card, CardImg } from "reactstrap";
 import LoginFormPage from "./components/loginForm";
+
 
 class SignInPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isDesktop: false,
+    };
+    this.updatePredicate = this.updatePredicate.bind(this);
+  }
+
+  componentDidMount() {
+    this.updatePredicate();
+    window.addEventListener("resize", this.updatePredicate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updatePredicate);
+  }
+
+  updatePredicate() {
+    this.setState({ isDesktop: window.innerWidth > 1000 });
   }
 
   render() {
+    const isDesktop = this.state.isDesktop;
     return (
-      <div className='loginBackground'>
-        <Card md={12} sm={12} className="loginContainer">
-          <Col md={7} sm={12} xs={12}>
-            <CardImg
-              width="100%"
-              className='loginImage'
-              // style={{ width: "auto", height: 900 }}
-              src={LoginImage}
-              alt="login image"
-            />
-          </Col>
+      <div className="loginBackground">
+        {isDesktop ? (
+          
+          <Card md={12} sm={12} className="loginContainer">
+                        <Col md={7} sm={12} xs={12}>
+              <CardImg
+                width="100%"
+                className="loginImage"
+                src={LoginImage}
+                alt="login image"
+              />
+            </Col>
+            <Col md={5} sm={12} xs={12} className="loginFormContainer">
+              <LoginFormPage />
+            </Col>
 
-          <Col md={5} sm={12} xs={12} className="loginFormContainer">
-            <LoginFormPage />
-          </Col>
-        </Card>
+            )
+          </Card>
+        ) : (
+          <Card md={12} sm={12} className="loginContainerMobile">
+            <Col md={12} sm={12} xs={12} className="loginFormContainer">
+              <LoginFormPage />
+            </Col>
+          </Card>
+        )}
       </div>
     );
   }
