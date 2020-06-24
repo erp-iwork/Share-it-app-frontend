d7df44124fa99e140a009cfc06d97c2937d5ee64
Yohannes Berhanu <Johnlights51@gmail.com>
2020-06-25 00:32:30 +0300

    Corrected Share FORM

---
+++ src/pages/chatting/components/ConversationList.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+import ConversationItem from '../conversation-item/conversationItem';
+
+import './ConversationList.scss';
+
+const ConversationList = ({ conversations, selectedConversation, onConversationItemSelected }) => {
+    const conversationItems = conversations.map((conversation) => {
+        const conversationIsActive = selectedConversation && conversation.id === selectedConversation.id;
+
+        return <ConversationItem 
+            key={ conversation.id }
+            onConversationItemSelected={ onConversationItemSelected }
+            isActive={ conversationIsActive }
+            conversation={ conversation } />;
+    });
+
+    return (
+        <div id="conversation-list">
+            {conversationItems}
+        </div>
+    );
+}
+
+export default ConversationList;
