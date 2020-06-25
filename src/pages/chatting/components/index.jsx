814a8f04e3cbd65e3b11e50dbf6df0d3512bc811
Yohannes Berhanu <Johnlights51@gmail.com>
2020-06-25 09:35:48 +0300

    Corrected Share FORM

--- src/pages/chatting/components/index.jsx
+++
@@ -1,8 +0,0 @@
-export { default as AttachmentIcon } from './AttachmentIcon';
-export { default as Buttons } from './Buttons';
-export { default as Chatform } from './Chat-form';
-export { default as ChatMain } from './/ChatMain';
-export { default as ChatTitle } from './ChatTitle';
-export { default as MainLayout } from './MainLayout';
-export { default as SubHeader } from './SubHeader';
-
