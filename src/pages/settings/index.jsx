7904a3d68339ba9adf0dfe8a79aeca8332f85d8e
AbrahamTerfie <abrahamterfie1236@gmail.com>
2020-06-26 10:55:49 +0300

    settings page tweak

--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -4,147 +4,147 @@ import Page from "../../components/Page";
 import CoverImage from "../../assets/background.jpg";
 
 import {
-  Col,
-  Form,
-  Card,
-  FormGroup,
-  Label,
-  Input,
-  Button,
-  Row,
-  CardHeader,
-  CardImg,
+    Col,
+    Form,
+    Card,
+    FormGroup,
+    Label,
+    Input,
+    Button,
+    Row,
+    CardHeader,
+    CardImg,
 } from "reactstrap";
 
 class Settings extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <Page breadcrumbs={[{ name: "person Name", active: true }]}>
-        <Card>
-          <Form className="form">
-            <Row>
-              <Col align="center" md={6} sm={12}>
-                <CardHeader> Update Profile Picture </CardHeader>
-                {/* <Avatar size={250} /> */}
-                <CardImg alt="" src={CoverImage} />
-                <Row>
-                  <Col>
-                    <Avatar size={150} />
-                  </Col>
-                  <Col align="center">
-                    <div className="changeProfile">
-                      <Button outline>Change background Picture</Button>
-                    </div>
-                    <div className="changeProfile">
-                      <Button outline>Change Picture</Button>
-                    </div>
-                  </Col>
-                </Row>
-              </Col>
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <Page breadcrumbs={[{ name: "person Name", active: true }]}>
+                <Card>
+                    <Form className="form">
+                        <Row className="containerBackground">
+                            <Col align="center" md={6} sm={12} >
+                                <CardHeader> Update Picture </CardHeader>
+                                {/* <Avatar size={250} /> */}
+                                <CardImg alt="" src={CoverImage} />
+                                <Row>
+                                    <Col>
+                                        <Avatar size={150} />
+                                    </Col>
+                                    <Col align="center">
+                                        <div className="changeProfile">
+                                            <Button outline>Change background Picture</Button>
+                                        </div>
+                                        <div className="changeProfile">
+                                            <Button outline>Change Picture</Button>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
 
-              <Col>
-                <CardHeader>Update Personal Information </CardHeader>
-                <FormGroup>
-                  <Label>Name</Label>
-                  <Col>
-                    <Input
-                      type="text"
-                      name="name"
-                      placeholder="Enter new user name "
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup>
-                  <Label>Email</Label>
-                  <Col>
-                    <Input
-                      type="email"
-                      name="email"
-                      placeholder="myemail@email.com"
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup>
-                  <Label>Location</Label>
-                  <Col>
-                    <Input
-                      type="text"
-                      name="name"
-                      placeholder="Enter new location name "
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="examplePassword">Password</Label>
-                  <Col>
-                    <Input
-                      type="password"
-                      name="password"
-                      placeholder=" New password "
-                    />
-                  </Col>
-                </FormGroup>
-                <CardHeader>Update contact Information </CardHeader>
-                <Row md={2}>
-                  <FormGroup>
-                    <Col>
-                      <Label>Phone number </Label>
+                            <Col>
+                                <CardHeader>Update Personal Information </CardHeader>
+                                <FormGroup>
+                                    <Label>Name</Label>
+                                    <Col>
+                                        <Input
+                                            type="text"
+                                            name="name"
+                                            placeholder="Enter new user name "
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Email</Label>
+                                    <Col>
+                                        <Input
+                                            type="email"
+                                            name="email"
+                                            placeholder="myemail@email.com"
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label>Location</Label>
+                                    <Col>
+                                        <Input
+                                            type="text"
+                                            name="name"
+                                            placeholder="Enter new location name "
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="examplePassword">Password</Label>
+                                    <Col>
+                                        <Input
+                                            type="password"
+                                            name="password"
+                                            placeholder=" New password "
+                                        />
+                                    </Col>
+                                </FormGroup>
+                                <CardHeader>Update contact Information </CardHeader>
+                                <Row md={2}>
+                                    <FormGroup>
+                                        <Col>
+                                            <Label>Phone number </Label>
 
-                      <Input
-                        type="number"
-                        name="phoneNumber"
-                        placeholder="Enter new Phone number  "
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup>
-                    <Col>
-                      <Label>Telegram username</Label>
+                                            <Input
+                                                type="number"
+                                                name="phoneNumber"
+                                                placeholder="Enter new Phone number  "
+                                            />
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Col>
+                                            <Label>Telegram username</Label>
 
-                      <Input
-                        type="text"
-                        name="telegramUN"
-                        placeholder="Enter new telegram user name "
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup>
-                    <Col>
-                      <Label>Facebook Adress</Label>
-                      <Input
-                        type="text"
-                        name="facebookUN"
-                        placeholder="Enter new Facebook user name "
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup>
-                    <Col>
-                      <Label>WhatsApp Adress </Label>
-                      <Input
-                        type="text"
-                        name="whatsAppUN"
-                        placeholder="Enter new WhatsApp user name "
-                      />
-                    </Col>
-                  </FormGroup>
-                </Row>
-                <Col className="submitbutton">
-                  <Button color="success" size="lg">
-                    Update Profile
+                                            <Input
+                                                type="text"
+                                                name="telegramUN"
+                                                placeholder="Enter new telegram user name "
+                                            />
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Col>
+                                            <Label>Facebook Adress</Label>
+                                            <Input
+                                                type="text"
+                                                name="facebookUN"
+                                                placeholder="Enter new Facebook user name "
+                                            />
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Col>
+                                            <Label>WhatsApp Adress </Label>
+                                            <Input
+                                                type="text"
+                                                name="whatsAppUN"
+                                                placeholder="Enter new WhatsApp user name "
+                                            />
+                                        </Col>
+                                    </FormGroup>
+                                </Row>
+                                <Col className="submitbutton">
+                                    <Button color="success" size="lg">
+                                        Update Profile
                   </Button>
-                </Col>
-              </Col>
-            </Row>
-          </Form>
-        </Card>
-      </Page>
-    );
-  }
+                                </Col>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Card>
+            </Page>
+        );
+    }
 }
 
 export default Settings;
