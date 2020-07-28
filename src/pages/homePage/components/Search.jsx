6cf7b56f0e82bab6f2d458fbe7bc33c5849e7516
Yohannes Berhanu <Johnlights51@gmail.com>
2020-07-28 11:40:59 +0300

    Added Search Popover

--- src/pages/homePage/components/Search.jsx
+++ src/pages/homePage/components/Search.jsx
@@ -1,20 +1,75 @@
 import React, { Component } from "react";
-import { Row, Col, Button, Input } from "reactstrap";
+// import { Row, Col, Button, Input } from "reactstrap";
 import SearchImage from "../../../assets/newResources/SearchImage.jpg";
 import { Spacer } from "../../../components/Layout";
 import { Link } from "react-router-dom";
 import routes from "../../../config/routes";
-
+import {
+  Popover,
+  PopoverBody,
+  Card,
+  Row,
+  Col,
+  Button,
+  Input,
+  CardBody,
+  CardHeader,
+} from "reactstrap";
+import { ProductsComp } from "./index";
 class HomePageSearch extends Component {
-  state = {};
+  state = {
+    isOpenSearchPopover: false,
+  };
+
+  toggleSearchPopover = (target) => {
+    this.setState({
+      target: target,
+      isOpenSearchPopover: !this.state.isOpenSearchPopover,
+      isOpenSearchCardPopover: false,
+    });
+  };
   render() {
     return (
       <>
+        <Popover
+          placement="bottom-start"
+          isOpen={this.state.isOpenSearchPopover}
+          toggle={this.toggleSearchPopover}
+          target="SearchPopover"
+          className="p-0"
+          style={{ minWidth: 950 }}
+        >
+          <PopoverBody
+          
+          className="border-light">
+            <Card>
+              <CardHeader>Search Results For "Search Input here"</CardHeader>
+              <CardBody>
+                <Row>
+                  <Col md={4}>
+                    <ProductsComp />
+                  </Col>
+                  <Col md={4}>
+                    <ProductsComp />
+                  </Col>
+                  <Col md={4}>
+                    <ProductsComp />
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </PopoverBody>
+        </Popover>
         <Row>
           <Col md={5} className="searchImageContainer">
             <img src={SearchImage} alt="" />
           </Col>
-          <Col md={7} align="center" className="searchformLayout">
+          <Col
+            md={7}
+            align="center"
+            className="searchformLayout"
+            id="SearchPopover"
+          >
             <h1>
               <b>
                 SHARE ANYTHING , <div>ANYWHERE ...</div>{" "}
